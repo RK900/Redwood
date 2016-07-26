0e024fc7318e1320178888ca3d4e4b1d416a558c
Rohan Koodli <rovik05@gmail.com>
2016-07-26 10:16:13 -0700

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
   packages = ['redwood'],
   version = '0.1',
   description='A library for dealing with trees',
-  author="Rohan Koodli"
+  author="Rohan Koodli",
   url='https://github.com/RK900/Redwood',
   keywords=['trees','datatypes','nodes'])
