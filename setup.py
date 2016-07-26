7b0653de4502501b328cc91a9830a22082677568
Rohan Koodli <rovik05@gmail.com>
2016-07-25 21:33:03 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+  name = 'redwood',
+  packages = ['redwood'],
+  version = '0.1',
+  description='A library for dealing with trees',
+  author="Rohan Koodli"
+  url='https://github.com/RK900/Redwood',
+  keywords=['trees','datatypes','nodes'])
